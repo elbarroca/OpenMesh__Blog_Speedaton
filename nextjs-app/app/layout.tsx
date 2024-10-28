9cc769b309be395335b94e212ebd8d9ca6ec5332
johnforfar <46335740+johnforfar@users.noreply.github.com>
2024-10-28 21:35:24 +0800

    removed google fonts imports

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -2,7 +2,6 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -11,12 +10,6 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -64,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className="scroll-smooth"
       suppressHydrationWarning
     >
       <link
@@ -94,7 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white font-sans">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
--- nix/package.nix
+++ nix/package.nix
@@ -1,11 +1,18 @@
-{ pkgs }:
+{ pkgs, lib ? pkgs.lib }:
 pkgs.buildNpmPackage {
   pname = "xnode-nextjs-blog-1";
   version = "1.0.0";
   src = ../nextjs-app;
 
   # FIXME: this needs to be updated every time the package-lock.json changes
-  npmDepsHash = "sha256-VNb9SCMYizYL2Vutp1l9s8W2up5qNvp/QhruqZvvqco=";
+  npmDepsHash = "sha256-SkoL9YyFxV9IFX81SBSKeLmpYNBMSrMSKMkSrUcnlMM=";
+
+  # Use fakeHash to get the correct hash
+  # npmDepsHash = lib.fakeHash;
+
+  # Add these lines to handle the dependency issues
+  npmFlags = [ "--legacy-peer-deps" ];
+  makeCacheWritable = true;
 
   postBuild = ''
     # Add a shebang to the server js file, then patch the shebang to use a
