d971e34dd0d35dc9f02bf97e7ded9235621f9f7c
Barroca <btcto154k@gmail.com>
2024-12-19 09:55:56 +0000

    Updated

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react'
 import { useEffect, useState } from 'react'
 import Main from './Main'
 import { Entry } from '@/types'
--- nix/package.nix
+++ nix/package.nix
@@ -1,18 +1,14 @@
 { pkgs, lib ? pkgs.lib }:
 pkgs.buildNpmPackage {
-  pname = "xnode-nextjs-blog-1";
+  pname = "Ricardo_Openmesh";
   version = "1.0.0";
   src = ../nextjs-app;
 
-  # FIXME: this needs to be updated every time the package-lock.json changes
-  npmDepsHash = "sha256-SkoL9YyFxV9IFX81SBSKeLmpYNBMSrMSKMkSrUcnlMM=";
+  npmDepsHash = lib.fakeHash;
 
-  # Use fakeHash to get the correct hash
-  # npmDepsHash = lib.fakeHash;
-
-  # Add these lines to handle the dependency issues
   npmFlags = [ "--legacy-peer-deps" ];
   makeCacheWritable = true;
+  NODE_OPTIONS = "--openssl-legacy-provider";
 
   postBuild = ''
     # Add a shebang to the server js file, then patch the shebang to use a
@@ -27,19 +23,16 @@ pkgs.buildNpmPackage {
     mkdir -p $out/{share,bin}
 
     cp -r .next/standalone $out/share/homepage/
-    # cp -r .env $out/share/homepage/
     cp -r public $out/share/homepage/public
 
     mkdir -p $out/share/homepage/.next
     cp -r .next/static $out/share/homepage/.next/static
 
-    # https://github.com/vercel/next.js/discussions/58864
     ln -s /var/cache/nextjs-app $out/share/homepage/.next/cache
 
     chmod +x $out/share/homepage/server.js
 
-    # we set a default port to support "nix run ..."
-    makeWrapper $out/share/homepage/server.js $out/bin/xnode-nextjs-blog-1 \
+    makeWrapper $out/share/homepage/server.js $out/bin/ricardo_openmesh \
       --set-default PORT 3000 \
       --set-default HOSTNAME 0.0.0.0
 
@@ -49,6 +42,6 @@ pkgs.buildNpmPackage {
   doDist = false;
 
   meta = {
-    mainProgram = "xnode-nextjs-blog-1";
+    mainProgram = "Ricardo_Openmesh";
   };
 }
